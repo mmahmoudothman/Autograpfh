a5d858f5c4a60e4ffb35583f488dab09c1f7d94b
mahmoud <mahmoudosman1819@gmail.com>
2018-06-09 15:50:25 +0200

    Init

--- app/src/main/java/com/example/osos/recyclinggraduationproject/MainActivity.java
+++ app/src/main/java/com/example/osos/recyclinggraduationproject/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.osos.recyclinggraduationproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -7,16 +11,18 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-; private WebView webView = null;
-
+    private WebView webView = null;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.webView = (WebView) findViewById(R.id.webview);
+        progressBar= (ProgressBar) findViewById(R.id.prograssbar);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -25,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(webViewClient);
 
         webView.loadUrl("http://me4u.herokuapp.com/");
-    }
+     }
 
 
     @Override
@@ -37,5 +43,43 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onKeyDown(keyCode, event);
     }
+
+
+
+
+
+    public class WebViewClientImpl extends WebViewClient {
+
+        private Activity activity = null;
+
+        public WebViewClientImpl(Activity activity) {
+            this.activity = activity;
+        }
+
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+
+        }
+
+
+
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            if(url.indexOf("http://me4u.herokuapp.com/") > -1 ) return false;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            activity.startActivity(intent);
+            return true;
+        }
+
+    }
+
 
 }
